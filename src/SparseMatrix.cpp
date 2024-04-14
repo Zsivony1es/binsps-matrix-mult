afb05a8696f03be7c9907bc464c6367e40e7f2e3
Peter Ivony <peterivony@protonmail.com>
2024-04-14 07:08:03 +0200

    Add three list initializer

--- src/SparseMatrix.cpp
+++ src/SparseMatrix.cpp
@@ -15,13 +15,12 @@ private:
     std::vector<T> entries;
     std::vector<size_t> col_index;
     std::vector<size_t> row_index;
-    size_t num_entries = 0;
     size_t col_count = 0;
 public:
     SparseMatrix(){}
 
     template <size_t N, size_t M>
-    SparseMatrix(Matrix<N, M, T> mx){
+    SparseMatrix(const& Matrix<N, M, T> mx){
         size_t num_elements = 0;
         for (size_t i = 0; i < N; ++i){
             for (size_t j = 0; j < M; ++j){
@@ -37,9 +36,18 @@ public:
             this->row_index.push_back(num_elements);
         }
     }
+
+    SparseMatrix(const& std::vector<T> entries, 
+                 const& std::vector<size_t> col_index,
+                 const& std::vector<size_t> row_index){
+        std::copy(entries.begin(), entries.end(), std::back_inserter(this->entries));
+        std::copy(col_index.begin(), col_index.end(), std::back_inserter(this->col_index));
+        std::copy(row_index.begin(), row_index.end(), std::back_inserter(this->row_index));
+        this->col_count = std::max_element(col_index.begin(), col_index.end()) + 1;
+    }
    
     T& operator[](size_t row, size_t col){
-        for (size_t i = 0; i < this->num_entries; ++i){
+        for (size_t i = 0; i < this->row_index.back(); ++i){
             if (this->col_index[i] == col && this->row_index[i] == row)
                 return this->entries[i];
         }
@@ -47,7 +55,7 @@ public:
     }
 
     const T& operator[](size_t row, size_t col) const {
-        for (size_t i = 0; i < this->num_entries; ++i){
+        for (size_t i = 0; i < this->row_index.back(); ++i){
             if (this->col_index[i] == col && this->row_index[i] == row)
                 return this->entries[i];
         }
