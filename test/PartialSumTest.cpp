52c9b3a62480fadac6db7e54a28389d70bbf6331
Peter Ivony <peterivony@protonmail.com>
2024-05-25 19:49:00 +0200

    Fix PartialSum algorithm

--- test/PartialSumTest.cpp
+++ test/PartialSumTest.cpp
@@ -5,9 +5,24 @@
 
 TEST(PartialSumTest, TestPartialSum) {
     std::vector<double> vec = {1.0, 2.0, 3.0, 4.0};
-    std::vector<double> expected = {0.0, 4.0, 3.0, 7.0, 2.0, 6.0, 5.0, 9.0, 1.0, 5.0, 4.0, 8.0, 3.0, 7.0, 6.0, 10.0};
+    std::unordered_map<size_t, double> expected = {
+        {0, 0.0}, {1, 1.0}, {2, 2.0}, {4, 3.0}, {8, 4.0}, {3, 3.0}, {5, 4.0}, {9, 5.0}, 
+        {6, 5.0}, {10, 6.0}, {12, 7.0}, {7, 6.0}, {11, 7.0}, {13, 8.0}, {14, 9.0}, {15, 10.0}
+    };
 
-    std::vector<double> result = PartialSum::precompute_partial_sums(vec);
+    auto result = PartialSum::precompute_partial_sums(vec);
+
+    ASSERT_EQ(result, expected);
+}
+
+TEST(PartialSumTest, TestPartialSumWithLimit) {
+    std::vector<double> vec = {1.0, 2.0, 3.0, 4.0};
+    std::unordered_map<size_t, double> expected = {
+        {0, 0.0}, {1, 1.0}, {2, 2.0}, {4, 3.0}, {8, 4.0}, 
+        {3, 3.0}, {5, 4.0}, {9, 5.0}, {6, 5.0}, {10, 6.0}, {12, 7.0}
+    };
+
+    auto result = PartialSum::precompute_partial_sums(vec, 2);
 
     ASSERT_EQ(result, expected);
 }
